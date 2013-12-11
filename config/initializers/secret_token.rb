db58dc6d3507f39f150fde290bef74f47ee2c5cc
Logan Yu <yu.logan@gmail.com>
2013-12-11 11:22:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Runblr::Application.config.secret_token = '9696647d0c21ae9d7c6b079a773d61ffa94df080de9175b5bc7af6dc3af86f6d0845ef56c5320c8a5587d78539fc61bac5e11d6fa55356ee53e4fcf0c8cf5298'
