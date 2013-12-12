e71f72354c2b4fc44bfdf20963934b2898cb0ae6
Logan Yu <yu.logan@gmail.com>
2013-12-12 13:52:27 -0500

    Added commenting to dashboard

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create]
 
   def create
-    @post = post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params[:comment])
     @comment.user = current_user
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,16 @@
 class Comment < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :body, :parent_comment_id
+
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :post, presence: true
+
+  belongs_to :post, inverse_of: :comments
+  belongs_to :user, inverse_of: :comments
+
+  has_many( :child_comments,
+           class_name: "Comment",
+           foreign_key: :parent_comment_id,
+           primary_key: :id)
+  belongs_to :parent_comment, class_name: "Comment", foreign_key: :parent_comment_id, primary_key: :id
 end
