f29479ea7ed6400fb2c0ed40c661db831a5a0509
Logan Yu <yu.logan@gmail.com>
2013-12-12 12:21:15 -0500

    Moved code out of dashboard into templates

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create]
+
+  def create
+    @post = post.find(params[:post_id])
+    @comment = @post.comments.build(params[:comment])
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to :back
+    else
+      flash[:errors] = @comment.errors.full_messages
+      redirect_to :back
+    end
+  end
+
+  def new
+    render :new
+  end
+
+end
