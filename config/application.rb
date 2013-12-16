8ce916c9da6c41fa79cfb2ee08af826745256630
Logan Yu <yu.logan@gmail.com>
2013-12-16 18:58:33 -0500

    added precompile line

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ end
 module Runblr
   class Application < Rails::Application
 
+    config.assets.initialize_on_precompile = false
 
 
     # Settings in config/environments/* take precedence over those specified here.
