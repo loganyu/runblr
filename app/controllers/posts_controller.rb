a0273df3af5763b727de99ea0980e2e646d70919
Logan Yu <yu.logan@gmail.com>
2014-01-03 00:49:50 -0500

    removed js from new post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,16 +6,16 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.new(params[:post])
     if @post.save
-      # redirect_to :back
-      respond_to do |format|
-        format.html { render partial: "new_post_template", post: @post}
-      end
-    end
-    # else
-    #   flash.now[:errors] = @post.errors.full_messages
-    #   # redirect_to :back
-    #   head :ok
+      redirect_to :back
+    #   respond_to do |format|
+    #     format.html { render partial: "new_post_template", post: @post}
+    #   end
     # end
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      # redirect_to :back
+      head :ok
+    end
   end
 
   def new
